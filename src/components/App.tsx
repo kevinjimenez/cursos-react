a297d60fc45a4f756ac1b0e8550e154ff2eedc31
Kevin <kivito2009@hotmail.com>
2021-08-30 20:49:32 -0500

    librerias y mas

---
+++ src/components/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+    <div className="row">
+      <div className="col-sm-6" style={{
+        backgroundColor: 'red'
+      }}>
+        <h1>haha</h1>
+      </div>
+      <div className="col-sm-6" style={{
+        backgroundColor: 'blue'
+      }}>
+        <h1>haha</h1>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
