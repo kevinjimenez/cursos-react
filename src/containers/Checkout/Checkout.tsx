5cf0cc542ffe8b45fd58b730e221ee4372daf005
kevin jimenez <kivito2009@hotmail.com>
2021-09-08 09:17:34 -0500

    checkpit con state

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -11,9 +11,14 @@ function Checkout(props: any) {
         removeToCart(product)
     }
 
-    const handleSumTotal = (product: any) => () => {
-        const reducer = (acumulador: number, currentValue: any) => acumulador + currentValue.price
+    const handleSumTotal = () => {
+        console.log('entre')
+        const reducer = (acumulador: number, currentValue: any) => {
+            console.log(acumulador, currentValue)
+            return acumulador + currentValue.price
+        }
         const sum = cart.reduce(reducer, 0);
+        console.log(sum)
         return sum;
     }
 
@@ -24,31 +29,42 @@ function Checkout(props: any) {
     return (
         <>
             <div className="row">
-                <div className="col-sm-6">
-                    <h3>Listar pedido</h3>
-                    <div className="row text-center">
-                        <div className="col-sm-3">
-                            <h4>Item name</h4>
-                        </div>
-                        <div className="col-sm-3">
+
+                {cart.length > 0 ?
+                    <div className="col-sm-6">
+
+                        <h3>Listar pedido</h3>
+                        {cart.map((item: any) => (
+                            <div className="row text-center" key={item.id}>
+                                <div className="col-sm-3">
+                                    <h4>{item.title}</h4>
+                                </div>
+                                <div className="col-sm-3">
                                     <span>
-                                $10
+                                $ {item.price}
                             </span>
-                        </div>
-                        <div className="col-sm-3"/>
-                        <div className="col-sm-3">
-                            <span className="badge bg-primary">Eliminar</span>
-                        </div>
+                                </div>
+                                <div className="col-sm-3"/>
+                                <div className="col-sm-3">
+                                    <span className="badge bg-primary" onClick={handleRemove(item)}>Eliminar</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div> :
+                    <div className="col-sm-6">
+                        <h3>Sin pedidos</h3>
                     </div>
-                </div>
+                }
+                {cart.length > 0 &&
                 <div className="col-sm-6 text-center">
-                    <h3>Precio total: $ 10</h3>
+                    <h3>Precio total: $ {handleSumTotal()}</h3>
                     <div className="d-grid gap-2">
                         <button className='btn btn-outline-success' onClick={handleClick}>
                             Continuar pedido
                         </button>
                     </div>
                 </div>
+                }
             </div>
         </>
     )
