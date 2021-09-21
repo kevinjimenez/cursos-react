6be29cb94c4f325db9e1c17ab10492e4e1e83ebd
kevin jimenez <kivito2009@hotmail.com>
2021-09-21 15:32:30 -0500

    animation

--- frontend-comando/src/components/App/App.tsx
+++ frontend-comando/src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from '../../GlobalStyles';
 import { ListOfPhotoCard } from '../../ListOfPhotoCard/ListOfPhotoCard';
 import {ListOfCategories} from '../ListOfCategories/ListOfCategories'
+import { Logo } from '../Logo';
 
 function App() {
   return (
     <>
     <GlobalStyle />
+    <Logo/>
     <ListOfCategories />
     <ListOfPhotoCard />
     </>
--- frontend-comando/src/components/PhotoCard/styles.js
+++ frontend-comando/src/components/PhotoCard/styles.js
@@ -1,6 +1,19 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+
+export const fadeInKeyframes = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+`
 
 export const ImgWrapper = styled.div`
+animation: 1s ${fadeInKeyframes} ease;
   border-radius: 10px;
   display: block;
   height: 0;
