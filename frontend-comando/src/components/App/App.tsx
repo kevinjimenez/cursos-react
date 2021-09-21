6ecf03ff6124a076b89ce45faf9f9433dfb399d1
kevin jimenez <kivito2009@hotmail.com>
2021-09-21 12:17:19 -0500

    uso de stules component en vez de scss

---
+++ frontend-comando/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Category } from '../Category/Category';
+
+function App() {
+  return (
+    <>
+    <Category />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend-comando/src/components/Category/Category.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
+
+const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }: any)=> {
+    return (
+        <h1>hola categoria</h1>
+    )
+}
+
+export {Category}
