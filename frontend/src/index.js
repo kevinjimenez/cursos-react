eb738362e204b35a6ba04adb287cef70e9d51003
kevin jimenez <kivito2009@hotmail.com>
2021-09-21 11:43:41 -0500

    lin standar para lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// ReactDOM.render(
-//   <h1>Seguimos con el curso avanzado!</h1>,
-//   document.getElementById("app")
-// );
-
 ReactDOM.render(
-  "Seguimos con el curso avanzado",
+  <h1>Seguimos con el curso avanzado!</h1>,
   document.getElementById("app")
 );
+
+// sin babel
+// ReactDOM.render(
+//   "Seguimos con el curso avanzado",
+//   document.getElementById("app")
+// );
