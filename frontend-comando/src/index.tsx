b045a62f9532de7138292d67806a4730d37bba9b
kevin jimenez <kivito2009@hotmail.com>
2021-09-22 12:12:38 -0500

    config y lib para graphql

--- frontend-comando/src/index.tsx
+++ frontend-comando/src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+// inicializo el aplllo cliente para grapql
+const client = new ApolloClient({
+  uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql'
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
