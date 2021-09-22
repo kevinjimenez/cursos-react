ef6df156eb2b5e3b064432081fd8cf5c7d36bd3d
kevin jimenez <kivito2009@hotmail.com>
2021-09-22 10:33:49 -0500

    uso de useEffect para render compopnent

--- frontend-comando/src/components/ListOfCategories/ListOfCategories.tsx
+++ frontend-comando/src/components/ListOfCategories/ListOfCategories.tsx
@@ -1,16 +1,57 @@
+import React from "react";
 import { Category } from "../Category/Category";
 import { Item, List } from "./styles";
-import {categories} from '../../db.json'
+import axios from "axios";
 
 const ListOfCategories = () => {
+  const [categories, setCategories] = React.useState([]);
+  const [showFixed, setShowFixed] = React.useState(false);
+  React.useEffect(() => {
+    // const fetchMyAPI = async () => {
+    //   const response = await axios.get(
+    //     "https://petgram-server-edsf8xpy2.now.sh/categories"
+    //   );
+    //   console.log("respuesta por de la API");
+    //   console.log(response.data);
+    //   setCategories(response.data)
+    // };
+
+    // fetchMyAPI();
+    axios
+      .get("https://petgram-server-edsf8xpy2.now.sh/categories")
+      .then((response) => {
+        setCategories(response.data);
+      });
+  }, []);
+
+  // hooks para render el categories
+  React.useEffect(() => {
+    const onScroll = (e: any) => {
+      let newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed?: boolean) => {
+    return (
+      <List className={fixed ? "fixed" : ""}>
+        {categories.map((item: any) => (
+          <Item key={item.id}>
+            <Category {...item} />
+          </Item>
+        ))}
+      </List>
+    );
+  };
   return (
-    <List>
-      {categories.map((item: any) => (
-        <Item key={item.id} >
-          <Category {...item}/>
-        </Item>
-      ))}
-    </List>
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
 
