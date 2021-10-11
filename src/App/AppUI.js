e9851701fb5d2a3213da59367a9f49bd43a132da
kevin <kivito2009@hotmail.com>
2021-10-11 12:50:13 -0500

    clonar el proyecto

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,93 +1,59 @@
-import React from "react";
-import {TodoContext} from "../TodoContext/todoContext";
-import {TodoCounter} from "../components/TodoCounter/TodoCounter";
-import {TodoSearch} from "../components/TodoSearch/TodoSearch";
-import {TodoList} from "../components/TodoList/TodoList";
-import {TodoItem} from "../components/TodoItem/TodoItem";
-import {CreateTodoButon} from "../components/CreateTodoButon/CreateTodoButon";
-import {CrearTodo} from "../modal/crear-todo";
-import {TodoForm} from "../form/todo-form";
-
-// function AppUI({
-//                    error,
-//                    loading,
-//                    totalTodos,
-//                    completedTodos,
-//                    searchValue,
-//                    setSearchValue,
-//                    searchedTodos,
-//                    completeTodos,
-//                    deleteTodos
-//                }) {
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodosError } from '../TodosError';
+import { TodosLoading } from '../TodosLoading';
+import { EmptyTodos } from '../EmptyTodos';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
 
 function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
 
-    const {
-        error,
-        loading,
-        searchedTodos,
-        completeTodos,
-        deleteTodos,
-        openModal,
-        setOpenModal
-    } = React.useContext(TodoContext)
-    // return (
-    //     <React.Fragment>
-    //         <TodoCounter
-    //             total={totalTodos}
-    //             completed={completedTodos}
-    //         />
-    //         <TodoSearch
-    //             searchValue={searchValue}
-    //             setSearchValue={setSearchValue}
-    //         />
-    //         <TodoList>
-    //             {error && <p>boom valio hahahahah</p>}
-    //             {loading && <p>Estamos cargando, hahahah</p>}
-    //             {(!loading && !searchedTodos.length) && <p>Create este hahah</p>}
-    //             {searchedTodos.map(todo => (
-    //                 <TodoItem
-    //                     key={todo.text}
-    //                     text={todo.text}
-    //                     completed={todo.completed}
-    //                     onComplete={() => completeTodos(todo.text)}
-    //                     onDelete={() => deleteTodos(todo.text)}
-    //                 />
-    //             ))}
-    //         </TodoList>
-    //         <CreateTodoButon/>
-    //     </React.Fragment>
-    // )
+      <TodoList>
+        {error && <TodosError />}
+        {loading && <TodosLoading />}
+        {(!loading && !searchedTodos.length) && <EmptyTodos />}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
 
-    return (
-        <React.Fragment>
-            <TodoCounter/>
-            <TodoSearch/>
-            <TodoList>
-                {error && <p>boom valio hahahahah</p>}
-                {loading && <p>Estamos cargando, hahahah</p>}
-                {(!loading && !searchedTodos.length) && <p>Create este hahah</p>}
-                {searchedTodos.map(todo => (
-                    <TodoItem
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodos(todo.text)}
-                        onDelete={() => deleteTodos(todo.text)}
-                    />
-                ))}
-            </TodoList>
-            {!!openModal && (
-                <CrearTodo>
-                    <p>hahah modal </p>
-                    <TodoForm>
-                    </TodoForm>
-                </CrearTodo>
-            )}
-            <CreateTodoButon
-            setOpenModal={setOpenModal}/>
-        </React.Fragment>
-    )
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
 }
 
-export {AppUI}
+export { AppUI };
--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -1,62 +1,47 @@
-import React from "react";
+import React from 'react';
 
-function useLocalStorage(itemName, inicialValue) {
-    // estado error
-    const [error, setError] = React.useState(false)
-
-    // estado carga
-    const [loading, setLoading] = React.useState(true)
-
-
-    // react hook estado inicial stateFull
-    const [item, setItem] = React.useState(inicialValue)
-
-    // sin el arreglo vacio se renderixa cada vez q escuche un cambio
-    React.useEffect(() => {
-        console.log('aqui esta usanso hock useEfecct')
-        setTimeout(() => {
-
-            try {
-                const localStorageItem = localStorage.getItem(itemName);
-                let parseItem;
-
-
-                if (!localStorageItem) {
-                    localStorage.setItem(itemName, JSON.stringify(inicialValue))
-                    parseItem = inicialValue;
-                } else {
-                    parseItem = JSON.parse(localStorageItem);
-                }
-
-                // cambiando e estado
-                setItem(parseItem);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-
-        }, 1000)
-    },);
-
-
-    const saveItem = (newItem) => {
-
-        try {
-            const stringifiedItem = JSON.stringify(newItem);
-            localStorage.setItem(itemName, stringifiedItem)
-            setItem(newItem)
-        } catch (error) {
-            setError(error);
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+  
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
         }
-    }
 
-
-    // si se retorna mas de 2 elementos se retoena un object
-    return {
-        item,
-        saveItem,
-        loading
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+    }, 3000);
+  });
+  
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
     }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
 }
 
-export  {useLocalStorage}
+export { useLocalStorage };
