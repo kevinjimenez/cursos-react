1063ea8abe5278a5418481ca2a6e16f03a94503d
kevin <kivito2009@hotmail.com>
2021-10-11 13:03:58 -0500

    creacion de componentes falso para composicion

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+import React from "react";
+import { TodoProvider } from "../TodoContext";
+import { AppUI } from "./AppUI";
 
+// function App() {
+//   // return (
+//   //   <TodoProvider>
+//   //     <AppUI />
+//   //   </TodoProvider>
+//   // );
+// }
 
 function App() {
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader />
+      <TodoList />
+    </>
   );
+}
+
+function TodoHeader() {
+  return (
+    <>
+      <TodoCounter />
+      <TodoSearch />
+    </>
+  );
+}
+
+function TodoList() {
+  return (
+    <>
+      <TodoItem />
+    </>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem() {
+  return <p>TodoItem</p>;
 }
 
 export default App;
